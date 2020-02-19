5686ba5c7e6c4d66302ab37ee3f4d1868404a9b6
KirillFilipp <kirill_filipp@mail.ru>
2020-02-19 19:47:14 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!Array.isArray(array) || array.length == 0) {
+    return 0;
+  } else {
+    let result = array.sort((a, b) => a - b);
+    return result[0];
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!Array.isArray(array) || array.length == 0) {
+    return 0;
+  } else {
+    let result = array.sort((a, b) => a - b);
+    return result[result.length - 1];
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!Array.isArray(array) || array.length == 0) {
+    return 0;
+  } else {
+    let result = array.reduce((accumulator, currentValue) => accumulator + currentValue);
+    return result / array.length;
+  }
 }
